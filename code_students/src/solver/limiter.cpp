d24c8b104abab5528d44550ea10ef1e8e063f9a6
LV 703078 PS Parallele Programmierung <cb761160@login.lcc3.uibk.ac.at>
2024-04-11 13:59:42 +0200

    implement minmod limiter to choose minimum slope between mean values of next neighboring cells

--- code_students/src/solver/limiter.cpp
+++ code_students/src/solver/limiter.cpp
@@ -9,6 +9,18 @@ limiter_minmod::limiter_minmod(double theta) { this->theta = theta; }
 
 double limiter_minmod::compute(double first, double second, double third) {
 
-	// TBD by students
-	return 42.0;
+	double outval = 0.0;
+	if (first * second < 0 or first * third < 0) {
+		outval = 0.0;
+	} else {
+		if (first < 0) {
+			double tmp_max = std::max(first, second);
+			outval = std::max(tmp_max, third);
+		}
+		else {
+			double tmp_min = std::min(first, second);
+			outval = std::min(tmp_min, third);
+		}
+	}
+	return outval;
 }
