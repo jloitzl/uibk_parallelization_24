cd69fe085c917798314cf29327906aada8134eb1
jloitzl <johannes.loitzl@uibk.ac.at>
2024-04-18 12:45:17 +0200

    complete implementation of speed of sound and characteristic speeds

--- code_students/src/setup/physics.cpp
+++ code_students/src/setup/physics.cpp
@@ -130,23 +130,23 @@ void physics::get_lambda_min_max(double &lambda_min, double &lambda_max, const f
 	int index_density = fluid_left_cell.get_index_density();
 	int index_velocity_parallel = fluid_left_cell.get_index_v_x();
 	if (local_direction == parallelisation::direction::y) {
-		// TBD by students
+		index_velocity_parallel = fluid_left_cell.get_index_v_y();
 	} else if (local_direction == parallelisation::direction::z) {
-		// TBD by students
+		index_velocity_parallel = fluid_left_cell.get_index_v_z();
 	}
 
-	double density_left = 42.0;  // TBD by students
-	double density_right = 42.0; // TBD by students
+	double density_left = fluid_left_cell.fluid_data[index_density];
+	double density_right = fluid_right_cell.fluid_data[index_density];
 
 	double v_parallel_left = fluid_left_cell.fluid_data[index_velocity_parallel];
-	double v_parallel_right = 42.0; // TBD by students
+	double v_parallel_right = fluid_right_cell.fluid_data[index_velocity_parallel];
 
 	double pressure_left = get_pressure(fluid_left_cell);
-	double pressure_right = 42.0; // TBD by students
+	double pressure_right = get_pressure(fluid_right_cell);
 
 	double sound_speed_left = get_sound_speed(density_left, pressure_left);
-	double sound_speed_right = 42.0; // TBD by students
+	double sound_speed_right = get_sound_speed(density_right, pressure_right);
 
 	lambda_max = std::max(v_parallel_left + sound_speed_left, v_parallel_right + sound_speed_right);
-	lambda_min = 42.0; // TBD by students
+	lambda_min = std::min(v_parallel_left - sound_speed_left, v_parallel_right - sound_speed_right);
 }
