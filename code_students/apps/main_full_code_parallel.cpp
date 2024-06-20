24de5c0b077288410c66e79a23b7bd08c27c3731
jloitzl <johannes.loitzl@uibk.ac.at>
2024-06-20 12:58:43 +0200

    fill out parallelisation assignment for ssedov volume

--- code_students/apps/main_full_code_parallel.cpp
+++ code_students/apps/main_full_code_parallel.cpp
@@ -58,7 +58,7 @@ int main(int argc, char **argv) {
 	std::cout << " Hello from rank: " << rank;
 
 	std::vector<int> tasks(3);
-	tasks[0] = 4;
+	tasks[0] = 8;
 	tasks[1] = 1;
 	tasks[2] = 1;
 
@@ -66,36 +66,45 @@ int main(int argc, char **argv) {
 	mpi_handler parallel_stuff(tasks);
 
 	grid_3D global_grid(bound_low, bound_up, num_cells, 2);
-	
-	grid_3D my_grid = parallel_stuff.make_local_grid(global_grid);
+	grid_3D local_grid = parallel_stuff.make_local_grid(global_grid);
 
-	std::cout << " Anfang: " << my_grid.x_grid.get_center(0) << " " << my_grid.x_grid.get_left(0) << "\n";
-	int num = my_grid.get_num_cells(0);
-	std::cout << " Ende: " << my_grid.x_grid.get_center(num-1) << " " << my_grid.x_grid.get_left(num) << "\n";
+	std::cout << " Anfang: " << local_grid.x_grid.get_center(0) << " " << local_grid.x_grid.get_left(0) << "\n";
+	int num = local_grid.get_num_cells(0);
+	std::cout << " Ende: " << local_grid.x_grid.get_center(num-1) << " " << local_grid.x_grid.get_left(num) << "\n";
 
-	MPI_Finalize();
-	return 0;
 
 	// Get number of Sedov cells
 	Sedov_volume = 0.0;
-	int num_Sedov_cells = 0;
-	double volume_cell = my_grid.x_grid.get_dx() * my_grid.y_grid.get_dx() * my_grid.z_grid.get_dx();
+	int num_Sedov_cells_local = 0;
+	double volume_cell = local_grid.x_grid.get_dx() * local_grid.y_grid.get_dx() * local_grid.z_grid.get_dx();
 
 	for (int ix = rank; ix < (rank + 1); ++ix) {
-		double x_position = my_grid.x_grid.get_center(ix);
-		for (int iy = 0; iy < my_grid.get_num_cells(1); ++iy) {
-			double y_position = my_grid.y_grid.get_center(iy);
-			for (int iz = 0; iz < my_grid.get_num_cells(2); ++iz) {
-				double z_position = my_grid.z_grid.get_center(iz);
+		double x_position = local_grid.x_grid.get_center(ix);
+		for (int iy = 0; iy < local_grid.get_num_cells(1); ++iy) {
+			double y_position = local_grid.y_grid.get_center(iy);
+			for (int iz = 0; iz < local_grid.get_num_cells(2); ++iz) {
+				double z_position = local_grid.z_grid.get_center(iz);
 				double dist = sqrt(sim_util::square(x_position) + sim_util::square(y_position) + sim_util::square(z_position));
 				if (dist < 0.1) {
 					Sedov_volume += volume_cell;
-					num_Sedov_cells++;
+					num_Sedov_cells_local++;
 				}
 			}
 		}
 	}
-	std::cout << " Volume of Sedov region: " << Sedov_volume << " in " << num_Sedov_cells << " cells\n";
+
+	int num_Sedov_cells_global  = 0;
+	double Sedov_volume_global = 0.0;
+
+	// MPI communication for Sedov volume and corresponding number of cells
+	MPI_Allreduce(&num_Sedov_cells_local, &num_Sedov_cells_global, 1, MPI_INT, MPI_SUM, MPI_COMM_WORLD);
+	MPI_Allreduce(&Sedov_volume, &Sedov_volume_global, 1, MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD);
+
+
+	std::cout << " Volume of Sedov region: " << Sedov_volume_global << " in " << num_Sedov_cells_global << " cells\n";
+		
+	MPI_Finalize();
+	return 0;
 
 	// Now, I will create a HD fluid
 	fluid hd_fluid(parallelisation::FluidType::adiabatic);
